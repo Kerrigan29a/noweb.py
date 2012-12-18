24d452ae67b2bdba62cb3b5a44372fcbc955603b
Giel van Schijndel <me@mortis.eu>
2012-12-18 14:32:13 +0100

    Move starting of actual execution to __main__ section
    Signed-off-by: Giel van Schijndel <me@mortis.eu>

--- noweb.py
+++ noweb.py
@@ -16,28 +16,11 @@ parser = argparse.ArgumentParser('NoWeb command line options.')
 parser.add_argument('-R', '--chunk',  dest='chunk_name', metavar='CHUNK', required=True,                            help='name of chunk to write to stdout')
 parser.add_argument('infile',                            metavar='FILE',  type=argparse.FileType('r'),              help='input file to process, "-" for stdin')
 parser.add_argument('-o', '--output', dest='outfile',    metavar='FILE',  type=argparse.FileType('w'), default='-', help='file to output to, "-" for stdout')
-
-args = parser.parse_args()
 chunk_re         = re.compile(r'<<(?P<name>[^>]+)>>')
 chunk_def        = re.compile(chunk_re.pattern + r'=')
 chunk_at         = re.compile(r'^@@(?=\s|$)')
 chunk_end        = re.compile(r'^@(?=\s|$)')
 chunk_invocation = re.compile(r'^(?P<indent>\s*)' + chunk_re.pattern + r'\s*$')
-
-chunkName = None
-chunks = {}
-
-for line in args.infile:
-    match = chunk_def.match(line)
-    if match:
-        chunkName = match.group('name')
-        chunks[chunkName] = []
-    else:
-        if chunk_end.match(line):
-            chunkName = None
-        elif chunkName:
-            line = chunk_at.sub('@', line)
-            chunks[chunkName].append(line)
 
 def expand(chunkName, indent=""):
     for line in chunks[chunkName]:
@@ -51,5 +34,21 @@ def expand(chunkName, indent=""):
             else:
                 yield line
 
-for line in expand(args.chunk_name):
-    args.outfile.write(line)
+if __name__ == "__main__":
+    args = parser.parse_args()
+    chunkName = None
+    chunks = {}
+
+    for line in args.infile:
+        match = chunk_def.match(line)
+        if match:
+            chunkName = match.group('name')
+            chunks[chunkName] = []
+        else:
+            if chunk_end.match(line):
+                chunkName = None
+            elif chunkName:
+                line = chunk_at.sub('@', line)
+                chunks[chunkName].append(line)
+    for line in expand(args.chunk_name):
+        args.outfile.write(line)
